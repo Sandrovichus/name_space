ea61e4c664a23e71acd74ddc7cfcd60e46f95229
Alexander <veber21@rambler.ru>
2024-07-22 18:56:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
 
-    inner_function()
+    inner_function()  # сработает при вызове функции test_function()
 
 
 inner_function()  # при вызове вложенной функции возникает ошибка, так как эта функция не присутствтует в глобальном
